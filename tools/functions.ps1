ed19c1d90c0ec5a1601ccc193fd4120c3351b709
Robert Labrie <robert.labrie@gmail.com>
2015-05-07 14:28:28 -0400

    smarter? path

--- tools/functions.ps1
+++ tools/functions.ps1
@@ -13,6 +13,12 @@ function Get-RabbitMQPath
 {
     $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ"
     if (Test-Path "HKLM:\SOFTWARE\Wow6432Node\") { $regPath = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ" }
-    return Split-Path -Parent (Get-ItemProperty $regPath "UninstallString").UninstallString
+    $path = Split-Path -Parent (Get-ItemProperty $regPath "UninstallString").UninstallString
+    $version = (Get-ItemProperty $regPath "DisplayVersion").DisplayVersion
+
+    return "$path\rabbitmq_server-$version"
 }
 
+$p = Get-RabbitMQPath
+
+$p
