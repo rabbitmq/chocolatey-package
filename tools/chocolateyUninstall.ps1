19b473c2989241c32f926d8e88894d319a94061e
Robert Labrie <robert.labrie@gmail.com>
2015-05-11 11:15:14 -0400

    smarter uninstall

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,8 +1,12 @@
-﻿try {
-  start-process -wait "C:\Program Files (x86)\RabbitMQ Server\uninstall"
-
-  Write-ChocolateySuccess 'RabbitMQ'
-} catch {
-
-  Write-ChocolateySuccess 'RabbitMQ'
+﻿function Get-RabbitMQPath
+{
+    $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ"
+    if (Test-Path "HKLM:\SOFTWARE\Wow6432Node\") { $regPath = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ" }
+    $path = Split-Path -Parent (Get-ItemProperty $regPath "UninstallString").UninstallString
+    $version = (Get-ItemProperty $regPath "DisplayVersion").DisplayVersion
+    return "$path\rabbitmq_server-$version"
 }
+
+$rabbitPath = Get-RabbitMQPath
+
+start-process -wait "$rabbitPath\..\uninstall" "/S"
