85d373ec36a8b66863155134a48ac19ba3829d00
Robert Labrie <robert.labrie@gmail.com>
2015-05-12 14:00:22 -0400

    Removed unused bat file

--- tools/SetupRabbitMqManagement.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.1\sbin\rabbitmq-service.bat" stop
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.1\sbin\rabbitmq-plugins.bat" enable rabbitmq_management --offline
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.1\sbin\rabbitmq-service.bat" install
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.1\sbin\rabbitmq-service.bat" start
