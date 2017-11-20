e7cc6f20882b5bddb0bbbe363eaf490ba46a1861
Jawad Qureshi <JawadQ@firmglobal.com>
2017-11-20 09:47:45 +0100

    Update to 3.6.14

--- tools/chocolateyHelpers.ps1
+++ tools/chocolateyHelpers.ps1
@@ -24,7 +24,7 @@ function Get-RabbitMQPath {
   if (Test-Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ") {
     $regPath = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ"
   }
-  
+
   if (Test-Path $regPath) {
     $path = Split-Path -Parent (Get-ItemProperty $regPath "UninstallString").UninstallString
     $version = (Get-ItemProperty $regPath "DisplayVersion").DisplayVersion
