31f9be84dee1e51aa9a90214571660433e2da198
pivotal-rabbitmq-ci <info@rabbitmq.com>
2022-04-27 07:55:05 +0000

    Update Chocolatey package from RabbitMQ version 3.8.29 to 3.8.30.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ if ($arguments['RABBITMQBASE']) {
   ${Env:RABBITMQ_BASE} = $arguments['RABBITMQBASE']
 }
 
-Install-ChocolateyPackage 'rabbitmq' 'EXE' '/S' 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.29/rabbitmq-server-3.8.29.exe'
+Install-ChocolateyPackage 'rabbitmq' 'EXE' '/S' 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.30/rabbitmq-server-3.8.30.exe'
 
 $rabbitPath = Get-RabbitMQPath
 if (-not $arguments.ContainsKey('NOMANAGEMENT')) {
