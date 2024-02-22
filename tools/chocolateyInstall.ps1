0323cf964b8dbf3a1371bdb8ad1594e54967cb24
Luke Bakken <luke@bakken.io>
2024-02-22 12:49:53 -0800

    Add command switches and required `-Checksum` argument
    (cherry picked from commit 4157e947b122daa41fb7d96ba2078f9e12704223)

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ if ($arguments['RABBITMQBASE']) {
   ${Env:RABBITMQ_BASE} = $arguments['RABBITMQBASE']
 }
 
-Install-ChocolateyPackage 'rabbitmq' 'EXE' '/S' 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.0/rabbitmq-server-3.13.0.exe'
+Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.0/rabbitmq-server-3.13.0.exe' -Checksum 'A3B1B54D4105BBA33CCEFC8EAD7CA10C0087148F1ED29B7FF9A39476501354FF'
 
 $rabbitPath = Get-RabbitMQPath
 if (-not $arguments.ContainsKey('NOMANAGEMENT')) {
