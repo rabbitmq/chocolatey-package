8fe37ea0af42ec218496e2d8893e346eae0b48f0
Luke Bakken <luke@bakken.io>
2024-02-22 16:56:05 -0800

    Bump to 3.13.99 to force re-publish

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ if ($arguments['RABBITMQBASE']) {
   ${Env:RABBITMQ_BASE} = $arguments['RABBITMQBASE']
 }
 
-Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.0/rabbitmq-server-3.13.0.exe' -ChecksumType sha256 -Checksum 'A3B1B54D4105BBA33CCEFC8EAD7CA10C0087148F1ED29B7FF9A39476501354FF'
+Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.99/rabbitmq-server-3.13.99.exe' -ChecksumType sha256 -Checksum 'A3B1B54D4105BBA33CCEFC8EAD7CA10C0087148F1ED29B7FF9A39476501354FF'
 
 $rabbitPath = Get-RabbitMQPath
 if (-not $arguments.ContainsKey('NOMANAGEMENT')) {
