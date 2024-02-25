e16e1c099009c3a72ae38bf5b657fc8112aa14d5
Luke Bakken <luke@bakken.io>
2024-02-25 14:15:15 -0800

    Try to address verification failure here - https://gist.github.com/choco-bot/61a17dfb880a664cd2494189cfb7d7e9

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -16,8 +16,10 @@ if ($arguments['RABBITMQBASE'])
 Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.0/rabbitmq-server-3.13.0.exe' -ChecksumType sha256 -Checksum a3b1b54d4105bba33ccefc8ead7ca10c0087148f1ed29b7ff9a39476501354ff
 
 $rabbitPath = Get-RabbitMQPath
+Write-Host "rabbitPath: $rabbitPat"
+Get-ChildItem env:\
+
 if (-not $arguments.ContainsKey('NOMANAGEMENT'))
 {
-    Start-Process -WhatIf -FilePath "$rabbitPath\sbin\rabbitmq-plugins.bat" -ArgumentList 'enable','rabbitmq_management' -NoNewWindow -Wait
-    Start-Process -FilePath "$rabbitPath\sbin\rabbitmq-plugins.bat" -ArgumentList 'enable','rabbitmq_management' -NoNewWindow -Wait
+    Start-Process -Verbose -FilePath "$rabbitPath\sbin\rabbitmq-plugins.bat" -ArgumentList 'enable','rabbitmq_management' -NoNewWindow -Wait
 }
