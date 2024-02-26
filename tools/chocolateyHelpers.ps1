cbcfb0e036695c2ffab6780b8d88e0b6d6ad79ba
Luke Bakken <luke@bakken.io>
2024-02-25 16:16:07 -0800

    Clean up powershell code, use Get-PackageParameters, uninstall string is now quoted

--- tools/chocolateyHelpers.ps1
+++ tools/chocolateyHelpers.ps1
@@ -1,33 +1,16 @@
-function Get-ChocolateyPackageParameters {
-  $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
-  $arguments = @{}
-  $packageParameters = ${Env:ChocolateyPackageParameters}
-
-  if ($packageParameters) {
-    if ($packageParameters -Match $match_pattern) {
-      $results = $packageParameters | Select-String $match_pattern -AllMatches
-      $results.matches | % {
-        $arguments.Add(
-        $_.Groups['option'].Value.Trim(),
-        $_.Groups['value'].Value.Trim())
-      }
-    } else {
-      throw "Package Parameters were found but were invalid (REGEX Failure)"
+function Get-RabbitMQPath
+{
+    $regPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ'
+    if (Test-Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ')
+    {
+        $regPath = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ'
     }
-  }
 
-  return $arguments
-}
-
-function Get-RabbitMQPath {
-  $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ"
-  if (Test-Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ") {
-    $regPath = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\RabbitMQ"
-  }
-
-  if (Test-Path $regPath) {
-    $path = Split-Path -Parent (Get-ItemProperty $regPath "UninstallString").UninstallString
-    $version = (Get-ItemProperty $regPath "DisplayVersion").DisplayVersion
-    return "$path\rabbitmq_server-$version"
-  }
+    if (Test-Path $regPath)
+    {
+        $uninstallString = (Get-ItemProperty $regPath "UninstallString").UninstallString.Trim('"')
+        $path = Split-Path -Parent $uninstallString
+        $version = (Get-ItemProperty $regPath "DisplayVersion").DisplayVersion
+        return "$path\rabbitmq_server-$version"
+    }
 }
--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -5,21 +5,18 @@
 
 . "$PSScriptRoot\ChocolateyHelpers.ps1"
 
-$arguments = Get-ChocolateyPackageParameters ${Env:ChocolateyPackageParameters}
+$pp = Get-PackageParameters
 
-if ($arguments['RABBITMQBASE'])
+if ($pp['RABBITMQBASE'])
 {
-  [System.Environment]::SetEnvironmentVariable("RABBITMQ_BASE", $arguments['RABBITMQBASE'], "Machine" )
-  ${Env:RABBITMQ_BASE} = $arguments['RABBITMQBASE']
+    [System.Environment]::SetEnvironmentVariable("RABBITMQ_BASE", $pp['RABBITMQBASE'], "Machine" )
+    ${Env:RABBITMQ_BASE} = $pp['RABBITMQBASE']
 }
 
 Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.0/rabbitmq-server-3.13.0.exe' -ChecksumType sha256 -Checksum a3b1b54d4105bba33ccefc8ead7ca10c0087148f1ed29b7ff9a39476501354ff
 
 $rabbitPath = Get-RabbitMQPath
-Write-Host "rabbitPath: $rabbitPat"
-Get-ChildItem env:\
-
-if (-not $arguments.ContainsKey('NOMANAGEMENT'))
+if (-not $pp.ContainsKey('NOMANAGEMENT'))
 {
     Start-Process -Verbose -FilePath "$rabbitPath\sbin\rabbitmq-plugins.bat" -ArgumentList 'enable','rabbitmq_management' -NoNewWindow -Wait
 }
