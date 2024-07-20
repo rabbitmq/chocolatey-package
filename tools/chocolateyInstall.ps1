a1516a6d4ee212f4ffec5797d1d459d7ea14ef50
pivotal-rabbitmq-ci <info@rabbitmq.com>
2024-07-20 06:28:45 +0000

    Update Chocolatey package from RabbitMQ version 3.13.4 to 3.13.5.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -13,7 +13,7 @@ if ($pp['RABBITMQBASE'])
     ${Env:RABBITMQ_BASE} = $pp['RABBITMQBASE']
 }
 
-Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.4/rabbitmq-server-3.13.4.exe' -ChecksumType sha256 -Checksum 1c754ad32e1d096839267dce4fdf3f934f49f7512c8cdbc4eeec73a846eeacae
+Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.5/rabbitmq-server-3.13.5.exe' -ChecksumType sha256 -Checksum c100aef15ef100628e1f43ec55ee05e8f46c3acd06ce060504dfb59cddd6c0e7
 
 $rabbitPath = Get-RabbitMQPath
 if (-not $pp.ContainsKey('NOMANAGEMENT'))
