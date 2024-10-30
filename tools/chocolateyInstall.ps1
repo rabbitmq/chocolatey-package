758db64228b6f3b5229818f8349c67f3bfb81523
Luke Bakken <luke@bakken.io>
2024-10-30 13:42:37 -0700

    Update 4.0.3 release URI and SHA256 checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ if ($pp['RABBITMQBASE'])
 }
 
 # https://github.com/rabbitmq/rabbitmq-server/releases/download/v4.x.y/rabbitmq-server-4.x.y.exe
-Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v4.0.2/rabbitmq-server-4.0.2.exe' -ChecksumType sha256 -Checksum cf8ce6c2ab6285eed65dd6c786cd433e90bfc54621451cea4a8b5c6a24c21a1d
+Install-ChocolateyPackage -PackageName 'rabbitmq' -FileType 'exe' -SilentArgs '/S' -Url 'https://github.com/rabbitmq/rabbitmq-server/releases/download/v4.0.3/rabbitmq-server-4.0.3.exe' -ChecksumType sha256 -Checksum f9140e4edd5aaa80c3bbfe7bdf91f9a7fa1707422dd98de132b017d3759504cb
 
 $rabbitPath = Get-RabbitMQPath
 if (-not $pp.ContainsKey('NOMANAGEMENT'))
