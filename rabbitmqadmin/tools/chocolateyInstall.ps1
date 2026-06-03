84820e0fc49f887d53b5db051c4ef8494e916ec4
RabbitMQ <info@rabbitmq.com>
2026-06-03 07:11:41 +0000

    AU: 1 updated - rabbitmqadmin [skip ci] https://gist.github.com/lukebakken/e62c53c31b3a971cefe413a751f4f446/e9a3148269a9e8a8629140140dce4fefb29283f0

--- rabbitmqadmin/tools/chocolateyInstall.ps1
+++ rabbitmqadmin/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$url = 'https://github.com/rabbitmq/rabbitmqadmin-ng/releases/download/v2.31.0/rabbitmqadmin-2.31.0-x86_64-pc-windows-msvc.zip'
-$checksum = '913b13de4b4c656f026bc7c107575196a4f56ea459dc9028e2c9594eef338bd2'
+$url = 'https://github.com/rabbitmq/rabbitmqadmin-ng/releases/download/v2.32.0/rabbitmqadmin-2.32.0-x86_64-pc-windows-msvc.zip'
+$checksum = 'e315089d1c9ed78b66d045c6032e5155a2cf1020751ad23604ab975fe6402d17'
 
 Install-ChocolateyZipPackage -PackageName 'rabbitmqadmin' -Url $url -UnzipLocation $toolsDir -ChecksumType 'sha256' -Checksum $checksum
